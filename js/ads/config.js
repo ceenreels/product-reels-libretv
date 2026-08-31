8e25c6c0883dfdb70eabc8f8b1425ddc23d8d3f0
Ferida Local Verification <verification@ferida.invalid>
2026-08-31 16:18:00 +0800

    feat: optimize JuicyAds placements and SEO crawlability

--- js/ads/config.js
+++ js/ads/config.js
@@ -11,7 +11,11 @@ export const ADS_CONFIG = {
             popunderCode: '<!-- JuicyAds PopUnders v3 Start --> <script type="text/javascript" src="https://js.juicyads.com/jp.php?c=4474w233t244u4r2p2c4337474&u=https%3A%2F%2Fwww.jumeitianxia.com%2F"></script> <!-- JuicyAds PopUnders v3 End -->',
             popunderScriptSrc: '',
             slotSnippets: {
-                'ad-juicy-responsive-banner': {
+                'ad-juicy-home-banner': {
+                    desktop: `<!-- JuicyAds v3.0 --> <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script> <ins id="1125830" data-width="728" data-height="90"></ins> <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1125830});</script> <!--JuicyAds END-->`,
+                    mobile: `<!-- JuicyAds v3.0 --> <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script> <ins id="1125774" data-width="300" data-height="50"></ins> <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1125774});</script> <!--JuicyAds END-->`
+                },
+                'ad-juicy-player-banner': {
                     desktop: `<!-- JuicyAds v3.0 --> <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script> <ins id="1125773" data-width="300" data-height="250"></ins> <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1125773});</script> <!--JuicyAds END-->`,
                     mobile: `<!-- JuicyAds v3.0 --> <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script> <ins id="1125774" data-width="300" data-height="50"></ins> <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1125774});</script> <!--JuicyAds END-->`
                 }
@@ -25,7 +29,8 @@ export const ADS_CONFIG = {
         'ad-responsive-banner': { provider: 'adsterra', elementId: 'ad-responsive-banner' },
         'ad-native-banner': { provider: 'adsterra', elementId: 'ad-native-banner' },
         'ad-square-banner': { provider: 'adsterra', elementId: 'ad-square-banner' },
-        'ad-juicy-responsive-banner': { provider: 'juicyads', elementId: 'ad-juicy-responsive-banner' },
+        'ad-juicy-home-banner': { provider: 'juicyads', elementId: 'ad-juicy-home-banner' },
+        'ad-juicy-player-banner': { provider: 'juicyads', elementId: 'ad-juicy-player-banner' },
         'ad-popunder-juicyads': { provider: 'juicyads', elementId: 'ad-popunder' }
     }
 };
